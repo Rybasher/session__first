eb34058da47dc26a8d77d8741ee3eefbdb1c4390
Rybasher <kirary99@gmail.com>
2020-12-14 13:01:29 +0200

    created form and add js file for task1

--- #src/js/swiper.js
+++ #src/js/swiper.js
@@ -7,4 +7,5 @@ var swiper = new Swiper('.swiper-container', {
 	  nextEl: '.swiper-button-next',
 	  prevEl: '.swiper-button-prev',
 	},
+	
  });
--- first/js/script.js
+++ first/js/script.js
@@ -185,6 +185,7 @@ var swiper = new Swiper('.swiper-container', {
 	  nextEl: '.swiper-button-next',
 	  prevEl: '.swiper-button-prev',
 	},
+	
  });;
 
 
